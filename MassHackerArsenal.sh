796aeb88fac04880e4c56f496d902d3e9ad06333
Ege Balcı <egebal1996@gmail.com>
2016-12-30 22:43:09 +0300

    Initial commit...

---
+++ MassHackerArsenal.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+
+sudo apt-get update
+sudo apt-get -y upgrade 
+sudo apt-get -y dist-upgrade
+
+
+# Ubuntu Repos...
+sudo apt-get install -y sqlmap aircrack-ng wifite golang bless hexedit nasm apache2 radare2 apktool rarcrack proxychains binwalk crunch espeak netdiscover rdesktop steghide libimage-exiftool-perl chromium-browser snmp fcrackzip wireshark hping3 deluge nmap dmitry nikto hydra medusa tor sshuttle virtualbox git vim javacc ruby python-pip wget curl mingw-* gem volatility upx scalpel tcpdump john masscan reaver capstone build-essential ruby-dev libpcap-dev unzip php7.0-* mysql-common mysql-client  libapache2-mod-php7.0 
+
+# Pip
+pip install capstone
+pip install slowloris
+pip install scapy
+
+# Gem
+sudo gem install bettercap
+
+cd ~/
+mkdir Toolkit
+cd Toolkit
+
+# Git Repos...
+
+git clone https://github.com/DhavalKapil/icmptunnel.git
+git clone https://github.com/sqlmapproject/sqlmap.git
+git clone https://github.com/davidpepper/fierce-domain-scanner.git
+git clone https://github.com/Mebus/cupp.git
+git clone https://github.com/digininja/CeWL.git
+git clone https://github.com/iagox86/dnscat2.git
+git clone https://github.com/ryhanson/phishery.git
+git clone https://github.com/JusticeRage/Manalyze.git
+git clone git://git.kali.org/packages/exploitdb.git
+git clone https://github.com/trustedsec/social-engineer-toolkit.git
+git clone https://github.com/roobixx/cowpatty.git
+git clone https://github.com/hashcat/hashcat.git
+git clone https://github.com/laramies/theHarvester.git
+git clone https://github.com/secretsquirrel/the-backdoor-factory.git
+git clone https://github.com/EnableSecurity/wafw00f.git
+git clone https://github.com/xmendez/wfuzz.git
+git clone https://github.com/pxb1988/dex2jar.git
+git clone git://git.kali.org/packages/dirbuster.git
+git clone https://github.com/deltaxflux/fluxion.git
+git clone git://git.kali.org/packages/joomscan.git
+git clone https://github.com/hexploitable/MEMSCAN.git
+git clone https://github.com/hiddenillusion/AnalyzePDF.git
+git clone https://github.com/smalot/pdfparser.git
+git clone https://github.com/jesparza/peepdf.git
+git clone https://github.com/reverse-shell/routersploit.git
+git clone https://github.com/google/sandbox-attacksurface-analysis-tools.git
+git clone https://github.com/sophron/wifiphisher.git
+git clone https://github.com/wpscanteam/wpscan.git
+git clone https://github.com/Yara-Rules/rules.git
+git clone git://github.com/cuckoosandbox/cuckoo.git
+git clone https://github.com/juansacco/exploitpack.git
+git clone https://github.com/BlackArch/webshells.git
+git clone https://github.com/EgeBalci/HERCULES.git
+git clone https://github.com/EgeBalci/Cminer.git
+git clone https://github.com/EgeBalci/The-Eye.git
+
+# Manual Installs
+
+# Metasploit
+# Geny Motion
+# IDA Pro
+# OllyDebug
+# Immunity Debugger
+# Brup Suite
+# Nessus
+# Netsparker
+# Docker
+# Nvidia Drivers
+# PyCharm
+# Clion
+# Visual Studio Code
+# Armitage
+# Atom
+# LordPE
+# Wpsoffice
+# Beef-XSS (Docker)
+# Soap UI
+# Open VAS
+ 
+
